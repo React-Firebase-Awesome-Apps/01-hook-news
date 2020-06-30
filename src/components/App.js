8000ae829871fb4f6c2988b018dc753528ece6cb
fotis tsakiris <footios76@gmail.com>
2020-06-30 13:26:49 +0300

    Navigation added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import CreateLink from "../components/Link/CreateLink";
+import Login from "../components/Auth/Login";
+import ForgotPassword from "../components/Auth/ForgotPassword";
+import SearchLinks from "../components/Link/SearchLinks";
+import LinkList from "../components/Link/LinkList";
+import LinkDetail from "../components/Link/LinkDetail";
+import Header from "./Header";
 
 function App() {
   return (
-    <div style={{ margin: "0 auto", textAlign: "center" }}>
-      <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
-    </div>
+    <BrowserRouter>
+      <div className="app-container">
+        <Header />
+      </div>
+      <div className="route-container">
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+          <Route path="/create" component={CreateLink} />
+          <Route path="/login" component={Login} />
+          <Route path="/forgot" component={ForgotPassword} />
+          <Route path="/search" component={SearchLinks} />
+          <Route path="/top" component={LinkList} />
+          <Route path="/new/:page" component={LinkList} />
+          <Route path="/link/:linkId" component={LinkDetail} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
