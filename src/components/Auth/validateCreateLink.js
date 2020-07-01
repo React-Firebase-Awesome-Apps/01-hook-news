977c159edc41b4b7dc5173a0adcc7439d248c754
fotis tsakiris <footios76@gmail.com>
2020-07-01 16:54:55 +0300

    firestore and create a links collection

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -5,7 +5,7 @@ export default function validateCreateLink(values) {
     if (!values.description) {
         errors.description = 'Description required'
     } else if (values.description.length < 10) {
-        errors.description = 'Description must be at least 6 characters'
+        errors.description = 'Description must be at least 10 characters'
     }
 
     //Url errors
--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useFormValidation from "../Auth/useFormValidation";
 import validateCreateLink from "../Auth/validateCreateLink";
+import FirebaseContext from "../../firebase/context";
 
 const INITIAL_STATE = {
   description: "",
@@ -8,6 +9,7 @@ const INITIAL_STATE = {
 };
 
 function CreateLink(props) {
+  const { firebase, user } = React.useContext(FirebaseContext);
   const { handleSubmit, handleChange, values, errors } = useFormValidation(
     INITIAL_STATE,
     validateCreateLink,
@@ -15,6 +17,24 @@ function CreateLink(props) {
   );
 
   function handleCreateLink() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      const { url, description } = values;
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName
+        },
+        votes: [],
+        comments: [],
+        created: Date.now()
+      };
+      firebase.db.collection("links").add(newLink);
+      props.history.push("/");
+    }
     console.log("link created");
   }
   return (
